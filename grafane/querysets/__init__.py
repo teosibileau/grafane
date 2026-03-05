2e0a55a7b2710f82ccdf02efaa2d669347b798d3
Teofilo Sibileau <teo.sibileau@gmail.com>
2026-03-05 15:18:36 -0300

    Isolate influxql queryset into a queryset module in prep for flux code

---
+++ grafane/querysets/__init__.py
@@ -0,0 +1,4 @@
+from .influxql import InfluxQLQuerySet
+from .exceptions import WrongArgumentType
+
+__all__ = ["InfluxQLQuerySet", "WrongArgumentType"]
--- grafane/querysets/influxql.py
+++ grafane/querysets/influxql.py
@@ -2,11 +2,7 @@ import copy
 import time
 import warnings
 
-
-class WrongArgumentType(Exception):
-    def __init__(self, message, errors=None):
-        super().__init__(message)
-        self.errors = errors
+from .exceptions import WrongArgumentType
 
 
 class InfluxQLQuerySet:
